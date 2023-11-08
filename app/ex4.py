0438122f531ab2e5427c6c488065e1348eb61428
강연중 <yeonjungkang@gang-yeonjung-ui-MacBookAir.local>
2023-11-08 16:05:19 +0900

    teamprac4

---
+++ app/ex4.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+
+@app.route('/')
+def input():
+    return render_template('input_1030.html')
+
+@app.route('/result',methods=['POST','GET'])
+def result():
+    if request.method =='POST':
+        result=dict()
+        result['Name']=request.form.get('name')
+        result['StudentNumber']=request.form.get('StudentNumber')
+        result['Gender']=request.form.get('Gender')
+        result['Major']=request.form.get('Major')
+        result['Languages']=" ".join(request.form.getlist('PL')) 
+        return render_template('result_1030.html',result=result)
+
+
+if __name__ =='__main__':
+    app.run(host="0.0.0.0",debug=True,port=100)
